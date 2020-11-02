591647e4ffd19727b69666703ad17b3463299257
RikoKami <orglary@gmail.com>
2020-11-02 02:31:43 -0300

    feat: config mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const resolvers = require('./resolvers');
 const typeDefs = require('./typedefs');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/graphqlnode", {
+  useNewUrlParser: true
+})
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => console.log(`O serve ta rodando em: ${url}`));
