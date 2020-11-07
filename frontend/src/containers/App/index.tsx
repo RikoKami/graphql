67bba6b24335d96e06f1a9a45276096697c49dab
RikoKami <orglary@gmail.com>
2020-11-06 23:56:46 -0300

    feat: add react-helmet

--- frontend/src/containers/App/index.tsx
+++ frontend/src/containers/App/index.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { Route, Switch } from "react-router-dom";
 import { Main } from "../Main";
 
 export const App = () => {
+  const pageTitle = "Graphql - Todolist";
+  const pageDescription = "My TodoList";
+
   return (
-    <React.Suspense fallback={null}>
-      <Switch>
-        <Route path="/" component={Main} />
-      </Switch>
-    </React.Suspense>
+    <>
+      <Helmet>
+        <title>{pageTitle}</title>
+        <meta name="description" content="" />
+        <meta property="og:url" content={window.location.pathname} />
+        <meta property="og:type" content={"page"} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDescription} />
+      </Helmet>
+      <React.Suspense fallback={null}>
+        <Switch>
+          <Route path="/" component={Main} />
+        </Switch>
+      </React.Suspense>
+    </>
   );
 };
